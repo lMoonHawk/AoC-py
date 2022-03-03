9b68d7f24f31bffb0ea9df1b9bfaaa1080cbe171
lMoonHawk <lmoonhawk@gmail.com>
2022-03-03 17:12:22 +0100

    Improve day_09 solutions

--- 2021/day_09.py
+++ 2021/day_09.py
@@ -1,101 +1,51 @@
 def part1():
 
-    rolling_rows = []
+    heightmap: list[list[int]] = []
+    risk_level = 0
 
-    def shift(l_in, mode="lag"):
-        """Lead or lag list, filling with a 9 where empty"""
-        if mode == "lag":
-            out = [9] + l_in[:-1]
-        elif mode == "lead":
-            out = l_in[1:] + [9]
-        return out
+    def is_low(i: int, j: int, heightmap: list[list[int]]) -> bool:
+        """ Test for current value against neighbors in the array"""
+        height = heightmap[i][j]
+        last_row = len(heightmap) - 1
+        last_col = len(heightmap[0]) - 1
 
-    def vect_sub(a: list[int], b: list[int]):
-        """Element-wise substraction"""
-        return [a_i - b_i for a_i, b_i in zip(a, b)]
+        # Top
+        if i - 1 >= 0 and heightmap[i - 1][j] <= height:
+            return False
+        # Right
+        if j + 1 <= last_col and heightmap[i][j + 1] <= height:
+            return False
+        # Bottom
+        if i + 1 <= last_row and heightmap[i + 1][j] <= height:
+            return False
+        # Left
+        if j - 1 >= 0 and heightmap[i][j - 1] <= height:
+            return False
 
-    def create_masks(l_in, l_top=[], l_bottom=[]):
-        """Substract list from a "lag", 'lead" version of itself,
-        and also top or bottom list where applicable"""
-        masks = {}
-        masks["left"] = vect_sub(l_in, shift(l_in, "lag"))
-        masks["right"] = vect_sub(l_in, shift(l_in, "lead"))
-
-        if l_bottom:
-            masks["bottom"] = vect_sub(l_in, l_bottom)
-        if l_top:
-            masks["top"] = vect_sub(l_in, l_top)
-
-        return masks
-
-    def check_masks(l_in, masks):
-        """Check if each masks are negative for each position
-        -> Height is lower than each of its neighbors.\n
-        Returns the score of the row"""
-        sum_row = 0
-
-        for i, height in enumerate(l_in):
-            masks_values = [mask[i] for mask in masks.values()]
-            if all(map(lambda x: x < 0, masks_values)):
-                sum_row += 1 + height
-
-        return sum_row
-
-    run_sum = 0
+        return True
 
     with open("2021/data/day_09.txt") as f:
-        for i, row_char in enumerate(f):
-
+        for row_char in f:
             row_char = row_char.strip()
+
             new_row = [int(height) for height in row_char]
-            # We only keep 3 rows in memory
-            rolling_rows.append(new_row)
+            heightmap.append(new_row)
 
-            if i == 0:
-                continue  # Not enough info to compare (row below)
+        for i, row in enumerate(heightmap):
+            for j, height in enumerate(heightmap[i]):
+                if is_low(i, j, heightmap):
+                    risk_level += height + 1
 
-            elif i == 1:
-                # Masks all give a comparison of height
-                # First row: left, right and bottom
-                masks = create_masks(
-                    l_in=rolling_rows[0],
-                    l_bottom=rolling_rows[1])
-                to_check = rolling_rows[0]
-
-            elif i > 1:
-                # Row in the middle: left, right, bottom and top
-                masks = create_masks(
-                    l_in=rolling_rows[1],
-                    l_bottom=rolling_rows[2],
-                    l_top=rolling_rows[0])
-
-                to_check = rolling_rows[1]
-            # Iterate through the masks, all comparison at
-            # a position must be negative for a height to
-            # be considered lower than all its neighbors
-            run_sum += check_masks(to_check, masks)
-
-            if len(rolling_rows) > 3:
-                rolling_rows.pop(0)
-
-        # Last row: left, right and top
-        masks = create_masks(
-                    l_in=rolling_rows[2],
-                    l_top=rolling_rows[1])
-
-        run_sum += check_masks(rolling_rows[2], masks)
-
-        print(run_sum)
+        print(risk_level)
 
 
 def part2():
 
-    # TODO: Check to see if casting is necessary
-    heightmap: list[list[int]] = []
+    heightmap: list[list[str]] = []
 
     def calc_basin(
-            heighmap: list[list[int]], visited: list[list[bool]],
-            basin: list[int], i: int, j: int):
+            heighmap: list[list[str]], visited: list[list[bool]],
+            basin: list[str], i: int, j: int):
         """ Visits a location and calls itself on any non-visited neighbors.\n
         Returns (tuple): the heights in the basin so far
         and the updated visited array """
@@ -106,45 +56,45 @@ def part2():
         last_col = len(heightmap[0]) - 1
 
         # Top
-        if i - 1 >= 0:
-            if not visited[i - 1][j] and heightmap[i - 1][j] != 9:
-                basin, visited = calc_basin(
-                    heightmap, visited, basin, i - 1, j)
+        if (i - 1 >= 0 and not visited[i - 1][j] and
+                heightmap[i - 1][j] != "9"):
+            basin, visited = calc_basin(
+                heightmap, visited, basin, i - 1, j)
         # Right
-        if j + 1 <= last_col:
-            if not visited[i][j + 1] and heightmap[i][j + 1] != 9:
-                basin, visited = calc_basin(
-                    heightmap, visited, basin, i, j + 1)
+        if (j + 1 <= last_col and not visited[i][j + 1] and
+                heightmap[i][j + 1] != "9"):
+            basin, visited = calc_basin(
+                heightmap, visited, basin, i, j + 1)
         # Bottom
-        if i + 1 <= last_row:
-            if not visited[i + 1][j] and heightmap[i + 1][j] != 9:
-                basin, visited = calc_basin(
-                    heightmap, visited, basin, i + 1, j)
+        if (i + 1 <= last_row and not visited[i + 1][j] and
+                heightmap[i + 1][j] != "9"):
+            basin, visited = calc_basin(
+                heightmap, visited, basin, i + 1, j)
         # Left
-        if j - 1 >= 0:
-            if not visited[i][j - 1] and heightmap[i][j - 1] != 9:
-                basin, visited = calc_basin(
-                    heightmap, visited, basin, i, j - 1)
+        if (j - 1 >= 0 and not visited[i][j - 1] and
+                heightmap[i][j - 1] != "9"):
+            basin, visited = calc_basin(
+                heightmap, visited, basin, i, j - 1)
 
         return (basin, visited)
 
     with open("2021/data/day_09.txt") as f:
         for row_char in f:
             row_char = row_char.strip()
-            # TODO: Check to see if casting is necessary
-            new_row = [int(height) for height in row_char]
+
+            new_row = [height for height in row_char]
             heightmap.append(new_row)
 
     rows_count = len(heightmap)
     cols_count = len(heightmap[0])
     visited = [[False] * cols_count for i in range(rows_count)]
 
-    # Basins sizes
-    basins: list[int] = []
+    # List of basins
+    basins: list[list[str]] = []
 
     for i, row in enumerate(heightmap):
         for j, height in enumerate(heightmap[i]):
-            if not visited[i][j] and height != 9:
+            if not visited[i][j] and height != "9":
                 # calc_basin returns (as a tuple)
                 # - The length of the calculated basin
                 # - The new visited array
@@ -160,5 +110,5 @@ def part2():
 
 
 if __name__ == '__main__':
-    # part1()
+    part1()
     part2()
