4b4f864f13c0b4367e8839847ae5344831eac727
lMoonHawk <99245455+lMoonHawk@users.noreply.github.com>
2023-11-26 11:43:29 +0100

    add setup for new year
    *creates empty inputs txt files in year/data/
    *creates scripts with boilerplate in year/

---
+++ setup_year.py
@@ -0,0 +1,55 @@
+import sys
+import os
+
+if len(sys.argv) != 2:
+    print(f"Error - expected 1 argument, got {len(sys.argv)-1}")
+    print("Usage = setup_year.py [year]")
+    sys.exit()
+
+year = sys.argv[1]
+
+try:
+    year = int(year)
+except ValueError:
+    print(f"Error - expected integer but {year} was provided")
+    print("Usage = setup_year.py [year]")
+    sys.exit()
+
+
+year_directory = os.path.join(os.getcwd(), str(year))
+data_directory = os.path.join(os.getcwd(), str(year), "data")
+
+try:
+    os.makedirs(data_directory)
+    print("Directories created successfully")
+except OSError as error:
+    print("Directories cannot be created")
+    sys.exit()
+
+for day in range(1, 26):
+    open(os.path.join(data_directory, f"day_{day:0>2}.txt"), "w").close()
+
+    template = (
+        f'with open("{year}/data/day_{day:0>2}.txt") as f:\n',
+        """    pass
+
+
+def part1():
+    return 0
+
+
+def part2():
+    return 0
+
+
+if __name__ == "__main__":
+    print(f"Part 1: {part1()}")
+    print(f"Part 2: {part2()}")
+    """,
+    )
+
+    with open(os.path.join(year_directory, f"day_{day:0>2}.py"), "w") as f:
+        f.writelines(template)
+
+print(f"{year}/ python scripts created successfully")
+print(f"{year}/data/ empty inputs created successfully")
