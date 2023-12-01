9009aa89e89e470d08a549fbe7920f8244e77582
lMoonHawk <99245455+lMoonHawk@users.noreply.github.com>
2023-12-01 21:09:18 +0100

    add 2019 day_23 solution

--- 2019/day_23.py
+++ 2019/day_23.py
@@ -112,7 +112,6 @@ def part1():
     queues = [[n] for n in range(50)]
     outputs = [[] for _ in range(50)]
 
-    k = 0
     while True:
         for n in range(50):
             if output := run_intcode(programs[n], queues[n] if queues[n] else [-1], state=states[n]):
@@ -122,7 +121,6 @@ def part1():
                     if addr == 255:
                         return y
                     queues[addr].extend([x, y])
-        k += 1
 
 
 def part2():
@@ -130,11 +128,11 @@ def part2():
     programs = [get_program() for _ in range(50)]
     queues = [[n] for n in range(50)]
     outputs = [[] for _ in range(50)]
-    idle = [0 for _ in range(50)]
+    idle = [False for _ in range(50)]
 
     nat = []
     last_y_nat = None
-    k = 0
+
     while True:
         for n in range(50):
             if queues[n]:
@@ -145,20 +143,20 @@ def part2():
                 empty_payload = True
 
             if output := run_intcode(programs[n], payload, state=states[n]):
-                idle[n] = 0
+                idle[n] = False
                 outputs[n].append(output)
 
                 if len(outputs[n]) == 3:
                     addr, x, y = outputs[n].pop(0), outputs[n].pop(0), outputs[n].pop(0)
                     if addr == 255:
                         nat = [x, y]
-                    queues[addr].extend([x, y])
+                    else:
+                        queues[addr].extend([x, y])
 
-            # A -1 input was consumed
             elif empty_payload and not payload:
-                idle[n] += 1
+                idle[n] = True
 
-        if all(i > 50 for i in idle):
+        if all(idle) and len(nat) == 2:
             x_nat, y_nat = nat.pop(0), nat.pop(0)
             queues[0].extend([x_nat, y_nat])
 
@@ -166,98 +164,7 @@ def part2():
                 return y_nat
             last_y_nat = y_nat
 
-        k += 1
-
-
-def test():
-    states = [{"pointer": 0, "base": 0, "consumed": 0} for _ in range(50)]
-    programs = [get_program() for _ in range(50)]
-    queues = [[n] for n in range(50)]
-    outputs = [[] for _ in range(50)]
-    idle = [0 for _ in range(50)]
-
-    k = 0
-    while True:
-        print(f"Iteration: {k}")
-        for n in range(50):
-            if queues[n]:
-                payload = queues[n]
-                empty_payload = False
-            else:
-                payload = [-1]
-                empty_payload = True
-
-            if output := run_intcode(programs[n], payload, state=states[n]):
-                print(f"Computer {n} outputs {output}")
-                idle[n] = 0
-                outputs[n].append(output)
-
-                if len(outputs[n]) == 3:
-                    addr, x, y = outputs[n].pop(0), outputs[n].pop(0), outputs[n].pop(0)
-                    if addr == 255:
-                        print(f"Computer {n} sends packet to nat: {x, y}")
-                        continue
-                    print(f"Computer {n} sends packet {x,y} to address {addr}")
-                    queues[addr].extend([x, y])
-
-            # A -1 input was consumed
-            elif empty_payload and not payload:
-                idle[n] += 1
-                print(f"Computer {n} consumed an empty input")
-
-        if all(i > 0 for i in idle):
-            print(f"Network is idle")
-
-        k += 1
-
-
-def test2():
-    states = [{"pointer": 0, "base": 0, "consumed": 0} for _ in range(50)]
-    programs = [get_program() for _ in range(50)]
-    queues = [[n] for n in range(50)]
-    outputs = [[] for _ in range(50)]
-    idle = [0 for _ in range(50)]
-
-    with open("2019/test.txt", "w") as f:
-        k = 0
-        while True:
-            f.write(f"Iteration: {k}\n")
-            for n in range(50):
-                if queues[n]:
-                    payload = queues[n]
-                    empty_payload = False
-                else:
-                    payload = [-1]
-                    empty_payload = True
-
-                if output := run_intcode(programs[n], payload, state=states[n]):
-                    f.write(f"Computer {n} outputs {output}\n")
-                    idle[n] = 0
-                    outputs[n].append(output)
-
-                    if len(outputs[n]) == 3:
-                        addr, x, y = outputs[n].pop(0), outputs[n].pop(0), outputs[n].pop(0)
-                        if addr == 255:
-                            f.write(f"Computer {n} sends packet to nat: {x, y}\n")
-                            continue
-                        f.write(f"Computer {n} sends packet {x,y} to address {addr}\n")
-                        queues[addr].extend([x, y])
-
-                # A -1 input was consumed
-                elif empty_payload and not payload:
-                    idle[n] += 1
-                    f.write(f"Computer {n} consumed an empty input\n")
-
-            if all(i > 0 for i in idle):
-                f.write(f"Network is idle\n")
-
-            k += 1
-            if k > 5000:
-                break
-
 
 if __name__ == "__main__":
-    # print(f"Part 1: {part1()}")
+    print(f"Part 1: {part1()}")
     print(f"Part 2: {part2()}")
-    # test()
-    # test2()
