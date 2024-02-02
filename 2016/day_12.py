22755605356c32f67ca5deb223f9bfdee05affad
lMoonHawk <99245455+lMoonHawk@users.noreply.github.com>
2024-02-02 12:01:49 +0100

    remove old code from 2016 day_12

--- 2016/day_12.py
+++ 2016/day_12.py
@@ -2,25 +2,13 @@ with open("2016/data/day_12.txt") as f:
     instructions = [line.strip().split() for line in f.readlines()]
 
 
-def evaluate(args, registers):
-    r = args[0]
-    values = []
-    for arg in args:
-        if not arg.strip("-").isnumeric():
-            values.append(registers[arg])
-        else:
-            values.append(int(arg))
-    return r, values[0], values[1] if len(values) == 2 else None
-
-
 def evaluate(arg, registers):
     if arg.strip("-").isnumeric():
         return int(arg)
     return registers[arg]
 
 
-def part1():
-    registers = {"a": 0, "b": 0, "c": 0, "d": 0}
+def run(instructions, registers):
     ip = 0
     while 0 <= ip < len(instructions):
         op, *args = instructions[ip]
@@ -34,25 +22,15 @@ def part1():
         elif op == "jnz":
             ip += -1 + evaluate(args[1], registers) if evaluate(args[0], registers) else 0
         ip += 1
-    return registers["a"]
+    return registers
+
+
+def part1():
+    return run(instructions, {"a": 0, "b": 0, "c": 0, "d": 0})["a"]
 
 
 def part2():
-    registers = {"a": 0, "b": 0, "c": 1, "d": 0}
-    ip = 0
-    while 0 <= ip < len(instructions):
-        op, *args = instructions[ip]
-
-        if op == "cpy":
-            registers[args[1]] = evaluate(args[0], registers)
-        elif op == "inc":
-            registers[args[0]] += 1
-        elif op == "dec":
-            registers[args[0]] -= 1
-        elif op == "jnz":
-            ip += -1 + evaluate(args[1], registers) if evaluate(args[0], registers) else 0
-        ip += 1
-    return registers["a"]
+    return run(instructions, {"a": 0, "b": 0, "c": 1, "d": 0})["a"]
 
 
 if __name__ == "__main__":
