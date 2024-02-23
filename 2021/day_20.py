6f20bdb47bfd717903b608a1872965e96cb48eb1
lMoonHawk <99245455+lMoonHawk@users.noreply.github.com>
2024-02-23 14:56:39 +0100

    improve and standardise 2021 day_20 solutions

--- 2021/day_20.py
+++ 2021/day_20.py
@@ -1,69 +1,40 @@
-def parse():
-    with open("2021/data/day_20.txt") as f:
-        input_image = []
-        for row, content in enumerate(f):
-            content = content.strip()
-
-            if not row:
-                algo = content
-                continue
-
-            if content != "":
-                input_image.append(list(content))
-
-    return algo, input_image
+with open("2021/data/day_20.txt") as f:
+    algo, input_img = f.read().strip().split("\n\n")
+    ALGO = [cell == "#" for cell in algo]
+    IMG = [[cell == "#" for cell in row] for row in input_img.split("\n")]
 
 
-def get_index(image, i, j, inf="."):
-    neighbors = [
-        (ni, nj) for ni in range(i - 1, i + 2) for nj in range(j - 1, j + 2)
-    ]
-    pixels = []
-    for ni, nj in neighbors:
-        if 0 <= ni <= len(image) - 1 and 0 <= nj <= len(image[0]) - 1:
-            pixels.append(image[ni][nj])
+def get_pixel(input_img, i, j, algo, infinity):
+    index = 0
+    for ni, nj in [(i + oi, j + oj) for oi in range(-1, 2) for oj in range(-1, 2)]:
+        index <<= 1
+        if 0 <= ni < len(input_img) and 0 <= nj < len(input_img[0]):
+            index += input_img[ni][nj]
         else:
-            pixels.append(inf)
-
-    number = "".join(["1" if pixel == "#" else "0" for pixel in pixels])
-    return int(number, 2)
+            index += infinity
+    return algo[index]
 
 
-def enhance(input_image, algo, inf):
-    output_image = []
-    for i in range(-1, len(input_image) + 1):
-        output_image.append([])
-        for j in range(-1, len(input_image[0]) + 1):
-            index = get_index(input_image, i, j, inf)
-            output_image[i + 1].append(algo[index])
-    return output_image
-
-
-def enhance_times(k):
-    algo, input_image = parse()
-
-    inf = "."
-    for k in range(k):
-        output_image = enhance(input_image, algo, inf)
-
-        if inf == ".":
-            inf = algo[0]
-        else:
-            inf = algo[-1]
-
-        input_image = output_image
-
-    return sum([sum([pixel == "#" for pixel in row]) for row in output_image])
+def enhance(k, input_img, algo):
+    invert = infinity = algo[0]
+    for _ in range(k):
+        infinity = infinity if not invert else (1 - infinity)
+        output_img = [
+            [get_pixel(input_img, i, j, algo, infinity) for j in range(-1, len(input_img[0]) + 1)]
+            for i in range(-1, len(input_img) + 1)
+        ]
+        input_img = output_img
+    return output_img
 
 
 def part1():
-    print(enhance_times(2))
+    return sum(sum(row) for row in enhance(2, IMG, ALGO))
 
 
 def part2():
-    print(enhance_times(50))
+    return sum(sum(row) for row in enhance(50, IMG, ALGO))
 
 
 if __name__ == "__main__":
-    part1()
-    part2()
+    print(f"Part 1: {part1()}")
+    print(f"Part 2: {part2()}")
