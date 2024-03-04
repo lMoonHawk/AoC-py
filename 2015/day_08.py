78aadbb08babedd88132899c6c15ba4d57f4ae42
lMoonHawk <99245455+lMoonHawk@users.noreply.github.com>
2024-03-04 12:58:59 +0100

    remove use of cursed eval function

--- 2015/day_08.py
+++ 2015/day_08.py
@@ -2,8 +2,20 @@ with open("2015/data/day_08.txt") as f:
     strings = [line.strip() for line in f]
 
 
+def eval_len(s):
+    s = s[1:-1]
+    count = k = 0
+    while k < len(s):
+        if s[k] == "\\":
+            k += 4 if s[k + 1] == "x" else 2
+        else:
+            k += 1
+        count += 1
+    return count
+
+
 def part1():
-    return sum(len(string) - len(eval(string)) for string in strings)
+    return sum(len(string) - eval_len(string) for string in strings)
 
 
 def part2():
