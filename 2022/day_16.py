f0eea6caa67b641d0fa969c1e0ed737b2e570fff
lMoonHawk <99245455+lMoonHawk@users.noreply.github.com>
2024-03-06 11:18:23 +0100

    add 2022 day_16 full solutions

--- 2022/day_16.py
+++ 2022/day_16.py
@@ -1,65 +1,93 @@
-def path(start, end):
-    visited = set()
-    queue = [[start, 0]]
+def get_cave():
+    with open("2022/data/day_16.txt") as f:
+        cave = dict()
+        flows = dict()
+        with open("2022/data/day_16.txt") as f:
+            for line in f:
+                flow, tunnels = line.strip().split("; ")
+                valve = line.split()[1]
+                cave[valve] = tunnels.replace("valves", "valve").split("valve ")[1].split(", ")
+                flows[valve] = int(flow.split("=")[1])
+        # Rename the valves with flow > 0 to specific set bits
+        names = dict()
+        for valve in list(cave):
+            if flows[valve] > 0:
+                name = 2 ** len(names)
+                names[valve] = name
+                flows[name] = flows.pop(valve)
+                cave[name] = cave.pop(valve)
+        for start, ends in cave.items():
+            cave[start] = [names[end] if end in names else end for end in ends]
 
+        return cave, flows
+
+
+def shortest_dist(graph, start, end):
+    visited = set()
+    queue = [(start, 0)]
     while queue:
         visit, dist = queue.pop(0)
         if visit == end:
             return dist
-        for tunnel in cave[visit]:
+        for tunnel in graph[visit]:
             if tunnel not in visited:
                 visited.add(tunnel)
-                queue.append([tunnel, dist + 1])
+                queue.append((tunnel, dist + 1))
 
 
-cave = dict()
-flows = dict()
-with open("2022/data/day_16.txt") as f:
-    for line in f:
-        flow, tunnels = line.strip().split("; ")
-        valve = line[6:8]
-        flow = int(flow.split("=")[1])
-        tunnels = (
-            tunnels.replace("valves", "valve").split("valve ")[1].split(", ")
-        )
-        cave[valve] = tunnels
-        flows[valve] = flow
+def get_distances(cave, flows):
+    return {
+        start: {end: shortest_dist(cave, start, end) for end in cave if flows[end] > 0 and end != start}
+        for start in cave
+        if start == "AA" or flows[start] > 0
+    }
 
-# Distances from all valve to all other
-distances = dict()
-for start in cave:
-    distances[start] = dict()
-    for end in cave:
-        distances[start][end] = path(start, end)
+
+def simulate(current, timer, distances, flows):
+    cache = dict()
+    pruning = dict()
+    best = 0
+    stack = []
+    stack.append((current, timer, 0, 0))
+    while stack:
+        current, timer, opened, total_flow = stack.pop()
+        best = max(best, total_flow)
+        if opened not in cache or total_flow > cache[opened]:
+            cache[opened] = total_flow
+        if (current, opened) in pruning and pruning[(current, opened)] > total_flow:
+            continue
+        pruning[(current, opened)] = total_flow
+
+        for valve, distance in distances[current].items():
+            if valve & opened:
+                continue
+            if (total_time := timer - distance - 1) < 0:
+                continue
+            stack.append((valve, total_time, opened + valve, total_flow + total_time * flows[valve]))
+    return best, cache
 
 
 def part1():
-    def simulate(current, opened=None, time=0):
-        if opened is None:
-            opened = set()
-
-        best_total = 0
-        total = 0
-        for valve, distance in distances[current].items():
-            if valve in opened or flows[valve] == 0:
-                continue
-            total_time = distance + 1 + time
-            if total_time <= 30:
-                current_opened = opened.union([valve])
-                flow = (30 - total_time) * flows[valve]
-                total = flow + simulate(valve, current_opened, total_time)
-                if total > best_total:
-                    best_total = total
-
-        return best_total
-
-    print(simulate("AA"))
+    cave, flows = get_cave()
+    dist = get_distances(cave, flows)
+    best_dist, _ = simulate("AA", 30, dist, flows)
+    return best_dist
 
 
 def part2():
-    pass
+    cave, flows = get_cave()
+    dist = get_distances(cave, flows)
+    _, cache = simulate("AA", 26, dist, flows)
+
+    # Search for the best disjointed combo of valve in the cache
+    return max(
+        own_flow + elephant_flow
+        for own, own_flow in cache.items()
+        for elephant, elephant_flow in cache.items()
+        if not own & elephant
+    )
 
 
 if __name__ == "__main__":
-    part1()
-    part2()
+    print(f"Part 1: {part1()}")
+    print(f"Part 2: {part2()}")
