43f2f24a609d35db9ffff315a35ad35c1e1222b7
lMoonHawk <99245455+lMoonHawk@users.noreply.github.com>
2024-03-18 15:49:36 +0100

    improve and standardise 2022 day_23 solutions

--- 2022/day_23.py
+++ 2022/day_23.py
@@ -1,98 +1,71 @@
-def adjacent(lat=None, lon=None):
-    if lat:
-        return [[lat, j] for j in range(-1, 2)]
-    return [[i, lon] for i in range(-1, 2)]
+def get_elves_pos_ini():
+    with open("2022/data/day_23.txt") as f:
+        return {(col, row) for row, line in enumerate(f) for col, char in enumerate(line) if char == "#"}
 
 
-# Check North, South, West and East cells
-checks = [
-    adjacent(lat=-1),
-    adjacent(lat=1),
-    adjacent(lon=-1),
-    adjacent(lon=1),
+CHECKS = [
+    [(x, -1) for x in range(-1, 2)],
+    [(x, 1) for x in range(-1, 2)],
+    [(-1, y) for y in range(-1, 2)],
+    [(1, y) for y in range(-1, 2)],
 ]
 
 
-def parse_elves():
-    elves = set()
-    with open("2022/data/day_23.txt") as f:
-        for row, line in enumerate(f):
-            for col, char in enumerate(line):
-                if char == "#":
-                    elves.add(tuple([row, col]))
-    return elves
+def dict_append(d, k, v):
+    if k not in d:
+        d[k] = []
+    d[k].append(v)
 
 
-def move_elves(elves, priority):
-    stage = dict()
-    for elf in elves:
-        # Checks in order of priority for the round
-        round_check = checks[priority:] + checks[:priority]
-        not_blocked = 0
-        for direction in round_check:
-            to_check = [
-                (elf[0] + direction_i[0], elf[1] + direction_i[1]) in elves
-                for direction_i in direction
-            ]
-            # Not blocked
-            if not any(to_check):
-                not_blocked += 1
-                # Propose move
-                if not_blocked == 1:
-                    stage[elf] = (
-                        elf[0] + direction[1][0],
-                        elf[1] + direction[1][1],
-                    )
-        # If not elves around, remove move
-        if not_blocked == 4:
-            del stage[elf]
+def move_elves(elves, checks):
+    moved = False
+    staging = dict()
+    for x, y in elves:
+        available = 0
+        for adjacents in checks:
+            empty_line = True
+            for mx, my in adjacents:
+                if (x + mx, y + my) in elves:
+                    empty_line = False
+                    break
+            if empty_line:
+                available += 1
+                if available == 1:
+                    mx, my = adjacents[1]
+                    move = x + mx, y + my
+            elif available > 0:
+                break
+        if 0 < available < 4:
+            dict_append(staging, move, (x, y))
 
-    # Go through proposed moves
-    for elf, move in stage.items():
-        # Only allow elves with unique moves to move
-        if list(stage.values()).count(move) == 1:
-            elves.remove(elf)
-            elves.add(move)
-
-    # Return True if no moves were done
-    return len(stage) == 0
+    for proposed, positions in staging.items():
+        if len(positions) == 1:
+            elves.remove(*positions)
+            elves.add(proposed)
+            moved = True
+    checks.append(checks.pop(0))
+    return moved
 
 
 def part1():
-    elves = parse_elves()
-    priority = 0
-
+    elves = get_elves_pos_ini()
+    checks = CHECKS.copy()
     for _ in range(10):
-        move_elves(elves, priority)
-        priority = (priority + 1) % 4
+        move_elves(elves, checks)
 
-    min_y, max_y, min_x, max_x = None, None, None, None
-    for elf in elves:
-        min_y = elf[0] if min_y is None or min_y > elf[0] else min_y
-        max_y = elf[0] if max_y is None or max_y < elf[0] else max_y
-        min_x = elf[1] if min_x is None or min_x > elf[1] else min_x
-        max_x = elf[1] if max_x is None or max_x < elf[1] else max_x
-
-    answer = (max_y - min_y + 1) * (max_x - min_x + 1) - len(elves)
-
-    print(answer)
+    xs, ys = [x for x, _ in elves], [y for _, y in elves]
+    return (max(xs) - min(xs) + 1) * (max(ys) - min(ys) + 1) - len(elves)
 
 
 def part2():
-    elves = parse_elves()
-    priority = 0
-    stage_count = 0
-    done = False
-
-    while not done:
-        if move_elves(elves, priority):
-            done = True
-        priority = (priority + 1) % 4
-        stage_count += 1
-
-    print(stage_count)
+    elves = get_elves_pos_ini()
+    checks = CHECKS.copy()
+    cnt = 0
+    while move_elves(elves, checks):
+        cnt += 1
+    return cnt + 1
 
 
 if __name__ == "__main__":
-    part1()
-    part2()
+    print(f"Part 1: {part1()}")
+    print(f"Part 2: {part2()}")
